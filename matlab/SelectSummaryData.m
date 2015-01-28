88fae38f9d620c01353fe5871789812f61a6ca7d
Einar Baumann <einar.baumann@gmail.com>
2015-01-28 16:49:59 +0100

    Add GUI for selecting properties to plot

---
+++ matlab/SelectSummaryData.m
@@ -0,0 +1,85 @@
+function [ properties ] = SelectSummaryData( summary_data )
+%SELECTSUMMARYDATA Allows the user to select the data to be plotted
+%   Uses a gui window with checkboxes to allow the user to select
+%   which of the available data to plot.
+
+    properties = struct; % Will hold the properties to be plotted
+
+    % Get root field names from summary data
+    root_names = fieldnames(summary_data);
+    
+    % Create and populate structure with all child field names
+    child_names = struct;
+    for i = 1:length(root_names)
+        tmp = fieldnames(getfield(summary_data, root_names{i}));
+        child_names.(root_names{i}) = tmp;
+    end            
+    
+    % Create figure as root gui element
+    fig_height = 800; fig_width = 900;
+    fh = figure('Visible','on','Position',[360,500,fig_width,fig_height]);
+    
+    % Create header label
+    uicontrol(fh, 'Style', 'text', ...
+        'String', 'Select properties to be plotted.', ...
+        'FontWeight', 'bold', ...
+        'Position', [25, fig_height-35, 500, 20]);
+    
+    % Create checkboxes for properties
+    width = 150; height = 20; x = 25;
+    for i = 1:length(root_names)
+        parent_value = root_names{i};
+        child_struct = getfield(child_names, parent_value);
+        for j = 1:length(child_struct)
+            y = fig_height - (j*20+50);
+            label = strcat(parent_value, ':', child_struct{j});
+            uicontrol(fh, 'Style', 'checkbox', ...
+                'String', label, ...
+                'Value', 0, 'Position', [x, y, width, height], ...
+                'Callback', @checkbox_clicked);
+        end
+        x = x + width + 25;
+    end
+    
+    
+    % A button used to close the window.
+    button_close = uicontrol(fh, 'Style', 'pushbutton', ...
+        'String', 'OK', 'Position', [390, 10, 50, 20], ...
+        'Callback', @okbutton_callback);
+    
+    waitfor(fh);
+
+    % Called when the OK button in the gui is clicked
+    function okbutton_callback(source, eventdata, handles)
+        close all;
+        return;
+    end
+
+    % Add or remove the property from the list of properties to be
+    % plotted when a checkbox is clicked, depending on the value
+    function checkbox_clicked(source, eventdata, handles)
+        property_name = get(source, 'String');
+        value = get(source, 'Value');
+        C = strsplit(property_name, ':');
+        parent = C{1};
+        child = C{2};
+        if value == 1
+            add_property(parent, child)
+        else
+            remove_property(parent, child)
+        end
+    end
+
+    function add_property(parent, child)
+        fprintf('Adding %s:%s\n', parent, child);
+        properties.(parent).(child) = summary_data.(parent).(child)
+        return;
+    end
+
+    function remove_property(parent, child)
+        fprintf('Removing %s:%s\n', parent, child)
+        return;
+    end
+
+end
+
--- matlab/SummaryPlotter.m
+++ matlab/SummaryPlotter.m
@@ -3,14 +3,28 @@
 % Allows you to save the plots as PDF documents or export the data
 % to a CSV format readable by Excel, Pgfplots (a LaTeX plotting package)
 % etc.
+clear all; close all; clc;
 
-%% Read Summary Data File(s)
+%% Get Path To Summary Data File
+[summary_file_path, folder_path, file_name, name, ext] = GetFilePath();
+
+    
+%% Read Summary Data
 % Use the _ReadEcliseSummary_ or _ReadAdgprsSummary_ function to read
-% headers and values from files. Requires the user to provide the path(s)
-% to the file(s).
+% headers and values from file. Which method is used is determined by
+% looking at the file name.
+
+if strcmp(ext, 'mat')
+    fprintf('Reading saved matlab variable (*.mat).\n');
+    summary_data = load(summary_file_path);
+else
+    fprintf('File extension/format not reconized. The allowed extensions')
+    fprintf('are .mat, .UNSMRY and .HDF5\');
+end
 
 %% Select Data To Be Plotted
 % Ask the user to select which of the available summary vectors to plot.
+properties_to_be_plotted = SelectSummaryData(summary_data)
 
 %% Create Plot
 % Create the plot. It will initially be hidden.
