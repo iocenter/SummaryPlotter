f233691ce85fba771bdf18147a0e5579e0d6d512
Einar Baumann <einar.baumann@gmail.com>
2015-01-29 10:29:07 +0100

    Extract label extraction to a function

--- matlab/SelectSummaryData.m
+++ matlab/SelectSummaryData.m
@@ -4,20 +4,13 @@ function [ properties ] = SelectSummaryData( summary_data )
 %   which of the available data to plot.
 
     properties = struct; % Will hold the properties to be plotted
-
-    % Get root field names from summary data
-    root_names = fieldnames(summary_data);
     
-    % Create and populate structure with all child field names
-    child_names = struct;
-    for i = 1:length(root_names)
-        tmp = fieldnames(getfield(summary_data, root_names{i}));
-        child_names.(root_names{i}) = tmp;
-    end            
+    [root_labels, child_labels] = ExtractLabels(summary_data);
     
     % Create figure as root gui element
     fig_height = 800; fig_width = 900;
-    fh = figure('Visible','on','Position',[360,500,fig_width,fig_height]);
+    x_pos = 50; y_pos = 50;
+    fh = figure('Visible','on','Position',[x_pos,y_pos,fig_width,fig_height]);
     
     % Create header label
     uicontrol(fh, 'Style', 'text', ...
@@ -27,26 +20,27 @@ function [ properties ] = SelectSummaryData( summary_data )
     
     % Create checkboxes for properties
     width = 150; height = 20; x = 25;
-    for i = 1:length(root_names)
-        parent_value = root_names{i};
-        child_struct = getfield(child_names, parent_value);
+    for i = 1:length(root_labels)
+        parent_value = root_labels{i};
+        child_struct = getfield(child_labels, parent_value);
         for j = 1:length(child_struct)
             y = fig_height - (j*20+50);
             label = strcat(parent_value, ':', child_struct{j});
             uicontrol(fh, 'Style', 'checkbox', ...
                 'String', label, ...
                 'Value', 0, 'Position', [x, y, width, height], ...
-                'Callback', @checkbox_clicked);
+                'Callback', @checkbox_callback);
         end
         x = x + width + 25;
     end
     
     
     % A button used to close the window.
-    button_close = uicontrol(fh, 'Style', 'pushbutton', ...
+    uicontrol(fh, 'Style', 'pushbutton', ...
         'String', 'OK', 'Position', [390, 10, 50, 20], ...
         'Callback', @okbutton_callback);
     
+    % Wait for the figure to be closed before returning from function
     waitfor(fh);
 
     % Called when the OK button in the gui is clicked
@@ -57,7 +51,7 @@ function [ properties ] = SelectSummaryData( summary_data )
 
     % Add or remove the property from the list of properties to be
     % plotted when a checkbox is clicked, depending on the value
-    function checkbox_clicked(source, eventdata, handles)
+    function checkbox_callback(source, eventdata, handles)
         property_name = get(source, 'String');
         value = get(source, 'Value');
         C = strsplit(property_name, ':');
@@ -71,14 +65,14 @@ function [ properties ] = SelectSummaryData( summary_data )
     end
 
     function add_property(parent, child)
-        properties.(parent).(child) = summary_data.(parent).(child)
+        properties.(parent).(child) = summary_data.(parent).(child);
         fprintf('Added %s:%s\n', parent, child);
         return;
     end
 
     function remove_property(parent, child)
         properties.(parent) = rmfield(properties.(parent), child);
-        if length(fieldnames(properties.(parent))) == 0
+        if isempty(fieldnames(properties.(parent)))
             properties = rmfield(properties, parent);
         end
         fprintf('Removed %s:%s\n', parent, child)
